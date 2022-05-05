60013424c5f16cf33d733b05965c61226b2cff12
Wilhem <wilhem.sorek@gmail.com>
2022-05-05 12:52:54 +0200

    màj page service

--- AppMobileGSB/AppMobileGSB/EchantillonServices.cs
+++ AppMobileGSB/AppMobileGSB/EchantillonServices.cs
@@ -18,12 +18,14 @@ namespace AppMobileGSB
 
             //Si elle existe déjà, ne pas recréer la base de données
             if (db != null)
+                
                 return;
 
                 var databasePath = Path.Combine(FileSystem.AppDataDirectory, "EchantillonDB.db");
                 db = new SQLiteConnection(databasePath);
 
                 db.CreateTable<Echantillon>();
+                 db.CreateTable<Composant>();
                 Console.WriteLine("Table créée");
         }
 
@@ -33,6 +35,19 @@ namespace AppMobileGSB
             lsech = db.Query<Echantillon>("select * from Echantillon");
             return lsech;
 
+        }
+        public static List<Composant> QuerySelectListComp(string comp, string id)
+        {
+            List<Composant> lsech = new List<Composant>();
+            lsech = db.Query<Composant>("select * from Composant where nom = ? and idEchantillon = ? ", comp, id);
+            return lsech;
+        }
+
+        public static List<Composant> QuerySelectListCompById(string id)
+        {
+            List<Composant> lsech = new List<Composant>();
+            lsech = db.Query<Composant>("select * from Composant where idEchantillon = ? ", id);
+            return lsech;
         }
 
 
@@ -49,6 +64,13 @@ namespace AppMobileGSB
             var id = db.Insert(echantillon);
         }
 
+        public static async Task addComposant(string nomComp, String idEchantillon)
+        {
+            await Init();
+
+            await insertComp(nomComp, idEchantillon);
+
+        }
         public static async Task majEchantillon(string unCode, int uneQte)
         {
 
@@ -60,6 +82,24 @@ namespace AppMobileGSB
                 db.Query<Echantillon>("update Echantillon SET quantiteStock = ? where code = ?", addition, unCode);
         }
 
+        public static async Task<Boolean> insertComp(string comp, string id)
+        {
+            Boolean result = false ;
+            await Init();
+            string query = "insert INTO Composant (nom, idEchantillon) values " + comp + "," + id + ";";
+            try
+            {
+                db.Execute(query);
+                result = true;
+            }
+            catch(SQLiteException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+
+
         public static async Task DelEchantillon(string unCode, int uneQte)
         {
             int suppres;
@@ -67,6 +107,13 @@ namespace AppMobileGSB
             suppres = qte[0].quantiteStock - uneQte;
             db.Query<Echantillon>("update Echantillon SET quantiteStock = ? where code = ?", suppres, unCode);
         }
+
+        public static async Task DelComp(string comp, string id)
+        {
+            
+           db.Query<Echantillon>("DELETE from Composant where nom = ? and idEchantillon = ?", comp, id);
+        }
+
         public static Boolean VerifQte(string unCode, int uneQte)
         {
             int suppres;
@@ -83,5 +130,6 @@ namespace AppMobileGSB
         {
             return (IEnumerable<Echantillon>)db.Query<Echantillon> ("select * from Echantillon where code = ?", echantillon.code);
         }
+       
     }
 }
